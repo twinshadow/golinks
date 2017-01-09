68dcd2c98a6b72a442d36a82fe341332b7712703
James Mills <prologic@shortcircuit.net.au>
2017-01-09 01:31:10 -0800

    Fixed gdef bookmark

--- defaults.go
+++ defaults.go
@@ -18,7 +18,7 @@ func init() {
 		"py3":  "https://docs.python.org/3/search.html?q=%s",
 		"yt":   "http://www.youtube.com/results?search_type=search_videos&search_sort=relevance&search_query=%s&search=Search",
 		"gim":  "https://www.google.com/search?q=%s&um=1&ie=UTF-8&hl=en&tbm=isch",
-		"gdef": "http://www.google.com/search?q=define%3A+%s&hl=en&lr=&oi=definel&defl=all",
+		"gdef": "http://www.google.com/search?q=define%%3A+%s&hl=en&lr=&oi=definel&defl=all",
 		"imdb": "http://www.imdb.com/find?q=%s",
 		"gm":   "http://maps.google.com/maps?q=%s",
 	}
